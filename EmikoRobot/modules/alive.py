612fdf9f4cbe66e5e5e9373926f385c75c558d15
PayXr <103725749+PayXr@users.noreply.github.com>
2022-06-23 15:43:07 +0700

    Initial commit

---
+++ EmikoRobot/modules/alive.py
@@ -0,0 +1,24 @@
+import os
+import re
+from platform import python_version as kontol
+from telethon import events, Button
+from telegram import __version__ as telever
+from telethon import __version__ as tlhver
+from pyrogram import __version__ as pyrover
+from EmikoRobot.events import register
+from EmikoRobot import telethn as tbot
+
+
+PHOTO = "https://telegra.ph/file/33a9a4c8bbc067e246e15.jpg"
+
+@register(pattern=("/alive"))
+async def awake(event):
+  TEXT = f"**Hi [{event.sender.first_name}](tg://user?id={event.sender.id}), I'm Kichiroô Robot.** \n\n"
+  TEXT += "⚪ **I'm Working Properly** \n\n"
+  TEXT += f"⚪ **My Master : [Bryyleee](https://t.me/bryyleee)** \n\n"
+  TEXT += f"⚪ **Library Version :** `{telever}` \n\n"
+  TEXT += f"⚪ **Telethon Version :** `{tlhver}` \n\n"
+  TEXT += f"⚪ **Pyrogram Version :** `{pyrover}` \n\n"
+  TEXT += "**Thanks For Adding Me Here ❤️**"
+  BUTTON = [[Button.url("Help", "https://t.me/kichirobot?start=help"), Button.url("Support", "https://t.me/KichiroSupport")]]
+  await tbot.send_file(event.chat_id, PHOTO, caption=TEXT,  buttons=BUTTON)
