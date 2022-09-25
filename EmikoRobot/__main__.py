0787b785a0477904f1d4263b4dc9fb83361836de
CilikProject <114132441+CilikProject@users.noreply.github.com>
2022-09-26 04:55:59 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -117,7 +117,7 @@ Click on the button bellow to get description about specifics command."""
 DONATE_STRING = """Heya, glad to hear you want to donate!
  You can support the project by contacting @Kingsagapung \
  Supporting isnt always financial! \
- Those who cannot provide monetary support are welcome to help us develop the bot at ."""
+ Those who cannot provide monetary support are welcome to help us develop the bot act ."""
 
 IMPORTED = {}
 MIGRATEABLE = []
