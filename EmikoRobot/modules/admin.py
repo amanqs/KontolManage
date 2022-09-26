ac6033cdea209b6bb2fb413007fca796eebbe675
CilikProject <114132441+CilikProject@users.noreply.github.com>
2022-09-26 10:59:42 -0700

    Update admin.py

--- EmikoRobot/modules/admin.py
+++ EmikoRobot/modules/admin.py
@@ -967,29 +967,29 @@ def bug_reporting(update: Update, _: CallbackContext):
 
 
 __help__ = """
-*Perintah Pengguna*:
-❂ /admins*:* daftar admin dalam obrolan
-❂ /pinned*:* untuk mendapatkan pesan yang disematkan saat ini.
-*Perintah Berikut hanya Admin:*
-❂ /pin*:* secara diam-diam menyematkan pesan yang dibalas - tambahkan `'loud'`` atau `'notify'` untuk memberikan notifikasi kepada pengguna
-❂ /unpin*:* melepas pin pesan yang sedang disematkan
-❂ /invitelink*:* mendapat invitelink
-❂ /promote*:* mempromosikan pengguna yang dibalas
-❂ /fullpromote*:* mempromosikan pengguna yang dibalas dengan hak penuh
-❂ /demote*:* mendemosikan balasan yang dibalas oleh pengguna
-❂ /title <title here>*:* menetapkan judul khusus untuk admin yang dipromosikan bot
-❂ /admincache*:* paksa refresh daftar admin
-❂ /del*:* menghapus pesan yang Anda balas
-❂ /purge*:* menghapus semua pesan antara ini dan pesan yang dibalas.
-❂ /purge <integer X>*:* menghapus pesan yang dibalas, dan X pesan yang mengikutinya jika membalas pesan.
-❂ /setgtitle <text>*:* atur judul grup
-❂ /setgpic*:* membalas gambar untuk ditetapkan sebagai foto grup
-❂ /setdesc*:* Setel deskripsi grup
-❂ /setsticker*:* Setel stiker grup
-*Aturan*:
-❂ /rules*:* dapatkan aturan untuk obrolan ini.
-❂ /setrules <aturan Anda di sini>*:* tetapkan aturan untuk obrolan ini.
-❂ /clearrules*:* hapus aturan untuk obrolan ini.
+*User Commands*:
+❂ /admins*:* list of admins in the chat
+❂ /pinned*:* to get the current pinned message.
+*The Following Commands are Admins only:* 
+❂ /pin*:* silently pins the message replied to - add `'loud'` or `'notify'` to give notifs to users
+❂ /unpin*:* unpins the currently pinned message
+❂ /invitelink*:* gets invitelink
+❂ /promote*:* promotes the user replied to
+❂ /fullpromote*:* promotes the user replied to with full rights
+❂ /demote*:* demotes the user replied to
+❂ /title <title here>*:* sets a custom title for an admin that the bot promoted
+❂ /admincache*:* force refresh the admins list
+❂ /del*:* deletes the message you replied to
+❂ /purge*:* deletes all messages between this and the replied to message.
+❂ /purge <integer X>*:* deletes the replied message, and X messages following it if replied to a message.
+❂ /setgtitle <text>*:* set group title
+❂ /setgpic*:* reply to an image to set as group photo
+❂ /setdesc*:* Set group description
+❂ /setsticker*:* Set group sticker
+*Rules*:
+❂ /rules*:* get the rules for this chat.
+❂ /setrules <your rules here>*:* set the rules for this chat.
+❂ /clearrules*:* clear the rules for this chat.
 """
 
 SET_DESC_HANDLER = CommandHandler("setdesc", set_desc, filters=Filters.chat_type.groups, run_async=True)
