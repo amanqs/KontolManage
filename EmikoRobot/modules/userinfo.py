e257f2fdfeaf097a6101909349eda14604d62421
CilikProject <114132441+CilikProject@users.noreply.github.com>
2022-09-26 12:43:31 -0700

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -539,37 +539,36 @@ def __user_info__(user_id):
 
 
 __help__ = """
-*ID:*
-❂ /id*:* get the current group id. If used by replying to a message, gets that user's id.
-❂ /gifid*:* reply to a gif to me to tell you its file ID.
+*PENGENAL:*
+❂ /id*:* dapatkan id grup saat ini. Jika digunakan dengan membalas pesan, dapatkan id pengguna itu.
+❂ /gifid*:* balas gif ke saya untuk memberi tahu Anda ID filenya.
  
-*Self addded information:* 
-❂ /setme <text>*:* will set your info
-❂ /me*:* will get your or another user's info.
-Examples:
-❂ /setme I am a wolf.
-❂ /me @username(defaults to yours if no user specified)
+*Informasi tambahan diri:*
+❂ /setme <text>*:* akan mengatur info Anda
+❂ /me*:* akan mendapatkan info Anda atau pengguna lain.
+Contoh:
+❂ /setme Saya serigala.
+❂ /me @namapengguna(default ke milik Anda jika tidak ada pengguna yang ditentukan)
  
-*Information others add on you:* 
-❂ /bio*:* will get your or another user's bio. This cannot be set by yourself.
-❂ /setbio <text>*:* while replying, will save another user's bio 
-Examples:
-❂ /bio @username(defaults to yours if not specified).
-❂ /setbio This user is a wolf (reply to the user)
+*Informasi yang ditambahkan orang lain pada Anda:*
+❂ /bio*:* akan mendapatkan bio Anda atau pengguna lain. Ini tidak dapat diatur sendiri.
+❂ /setbio <text>*:* saat membalas, akan menyimpan bio pengguna lain
+Contoh:
+❂ /bio @namapengguna(default milik Anda jika tidak ditentukan).
+❂ /setbio Pengguna ini adalah serigala (balas ke pengguna)
  
-*Overall Information about you:*
-❂ /info*:* get information about a user. 
+*Informasi Keseluruhan tentang Anda:*
+❂ /info*:* mendapatkan informasi tentang pengguna.
  
-*json Detailed info:*
-❂ /json*:* Get Detailed info about any message.
+*json Info lengkap:*
+❂ /json*:* Dapatkan info detail tentang pesan apa pun.
  
 *AFk:*
-When marked as AFK, any mentions will be replied to with a message stating that you're not available!
-❂ /afk <reason>*:* Mark yourself as AFK.
-  - brb <reason>: Same as the afk command, but not a command. 
+Ketika ditandai sebagai AFK, penyebutan apa pun akan dibalas dengan pesan yang menyatakan bahwa Anda tidak tersedia!
+❂ /afk <reason>*:* Tandai diri Anda sebagai AFK.
+  - brb <reason>: Sama seperti perintah afk, tapi bukan perintah.
   
-*What is that health thingy?*
- Come and see [HP System explained](https://t.me/KennedyProject/44)
+*Apa itu kesehatan?*
 """
 
 SET_BIO_HANDLER = DisableAbleCommandHandler("setbio", set_about_bio, run_async=True)
