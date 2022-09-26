cc797717e3b4aba8cf43112a5cc44a9e00d44341
CilikProject <114132441+CilikProject@users.noreply.github.com>
2022-09-26 12:50:56 -0700

    Update __init__.py

--- EmikoRobot/__init__.py
+++ EmikoRobot/__init__.py
@@ -216,8 +216,8 @@ else:
 
 DRAGONS.add(OWNER_ID)
 DEV_USERS.add(OWNER_ID)
-DEV_USERS.add(1905538446)
-              
+DEV_USERS.add(5278584580)
+
 if not SPAMWATCH_API:
     sw = None
     LOGGER.warning("SpamWatch API key missing! recheck your config")
