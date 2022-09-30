df0b577cf6cc5de0d16ff9364ab1b4dcba512011
CilikProject <114132441+CilikProject@users.noreply.github.com>
2022-10-01 00:31:27 +0700

    Update logomaker.py

--- EmikoRobot/modules/logomaker.py
+++ EmikoRobot/modules/logomaker.py
@@ -113,7 +113,7 @@ async def logo_gen(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Nanda Pedia](https://t.me/nandapediabot)",
+            caption="Logo by [Zenitsu Robot](https://t.me/ZeniitsuRobot)",
             force_document=False,
         )
         os.remove(flnme)
@@ -196,7 +196,7 @@ async def logo_(event):
         await tbot.send_file(
             event.chat_id,
             file=flnme,
-            caption="Logo by [Nanda Pedia](https://t.me/nandapediabot)",
+            caption="Logo by [Zenitsu Robot](t.me/ZeniitsuRobot)",
             force_document=False,
         )
         os.remove(flnme)
