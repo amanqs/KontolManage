c4e60eaa6a5e1ff549423f7512ec9e7ddf1d931c
CilikProject <114132441+CilikProject@users.noreply.github.com>
2023-01-03 16:30:08 +0700

    Update __main__.py

--- EmikoRobot/__main__.py
+++ EmikoRobot/__main__.py
@@ -366,7 +366,7 @@ def emiko_about_callback(update, context):
     if query.data == "emiko_":
         query.message.edit_text(
             text="🎧 Bantuan Perintah Music."
-            "\npilih menu di bawah ini untuk melihat bantuan Music J Robot ",
+            "\npilih menu di bawah ini untuk melihat bantuan Music Clay Bot",
             parse_mode=ParseMode.MARKDOWN,
             disable_web_page_preview=True,
             reply_markup=InlineKeyboardMarkup(
