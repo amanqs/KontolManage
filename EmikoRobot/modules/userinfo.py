fb1d0484521ef62553a26572134e04ab32300cc7
CilikProject <114132441+CilikProject@users.noreply.github.com>
2023-01-09 19:14:53 +0700

    Update userinfo.py

--- EmikoRobot/modules/userinfo.py
+++ EmikoRobot/modules/userinfo.py
@@ -461,7 +461,7 @@ def stats(update, context):
     db_size = SESSION.execute("SELECT pg_size_pretty(pg_database_size(current_database()))").scalar_one_or_none()
     uptime = datetime.datetime.fromtimestamp(boot_time()).strftime("%Y-%m-%d %H:%M:%S")
     botuptime = get_readable_time((time.time() - StartTime))
-    status = "╒═══「 *Babu Clay Robot Statistik Sistem*: 」\n\n"
+    status = "╒═══「 *Indo Music Bot Statistik Sistem*: 」\n\n"
     status += f"• *Waktu Mulai*: {str(uptime)}" + "\n"
     uname = platform.uname()
     status += f"• *Sistem*: {str(uname.system)}" + "\n"
@@ -485,7 +485,7 @@ def stats(update, context):
         update.effective_message.reply_text(status +
             "\n📊 *Statistik Bot*:\n"
             + "\n".join([mod.__stats__() for mod in STATS]) +
-            "\n\n╘══ 「Powered By: [Clay Robot](https://t.me/babuclaybot) 」\n\n",
+            "\n\n╘══ 「Powered By: [Indo Music](https://t.me/indomusic_bot) 」\n\n",
         parse_mode=ParseMode.MARKDOWN, reply_markup=InlineKeyboardMarkup(kontol), disable_web_page_preview=True)
     except BaseException:
         update.effective_message.reply_text(
@@ -495,9 +495,9 @@ def stats(update, context):
                         "\n📊 *Statistik Bot*:\n"
                         + "\n".join(mod.__stats__() for mod in STATS)
                     )
-                    + "\n\n📨 [ᴄʜᴀɴɴᴇʟ](https://t.me/diarygatchor) | 📣 [sᴜᴘᴘᴏʀᴛ](https://t.me/gatchorparty)\n\n"
+                    + "\n\n📨 [ᴄʜᴀɴɴᴇʟ](https://t.me/virtualinaja) | 📣 [sᴜᴘᴘᴏʀᴛ](https://t.me/virtualindoaja)\n\n"
                 )
-                + "╘══「 Powered By: [Clay Robot](t.me/babuclaybot) 」\n"
+                + "╘══「 Powered By: [Clay Robot](t.me/indomusic_bot) 」\n"
             ),
             parse_mode=ParseMode.MARKDOWN,
             reply_markup=InlineKeyboardMarkup(kontol),
